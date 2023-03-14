30195ad67afbe4698eeab79bc0e2a6fb8b9d6ab5
Alvin <alvin.varghese@habilelabs.io>
2023-03-14 18:20:39 +0530

    final commit

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const prompt = require('prompt-sync')();
 const events = require("events");
 const { eventEmitter }= require("./emitter.js")
 
-const numOne = +prompt("Enter First Number ")
-const numTwo = +prompt("Enter Second Number ")
-const opt = prompt("Enter the operation (add, sub, multi, divide) ");
 
-eventEmitter.emit(opt, numOne, numTwo)
+
+
+
+const validation=()=>{
+    const numOne = prompt("Enter First Number ")
+    if(isNaN(Number(numOne)) || numOne.includes(' ')){
+        return console.log("Please enter correct number1");
+    }
+    const numTwo = prompt("Enter Second  Number ")
+    if(isNaN (Number(numTwo)) || numTwo.includes(' ')){
+        return console.log("Please enter correct number2");
+    }
+    const opt = prompt("Enter the operation (add, sub, multi, divide) ");
+    if(opt=="add"|| opt=="sub"|| opt=="multi"|| opt== "divide" ){
+        return eventEmitter.emit(opt, numOne, numTwo) ;
+    }
+    return console.log("Enter valid operator");
+}
+
+validation();
--- operation.js
+++ operation.js
@@ -1,7 +1,7 @@
-const add = (a,b) => console.log(a+b);
-const sub = (a,b) => console.log(a-b);
-const mul = (a,b) => console.log(a*b);
-const div = (a,b) => console.log(a/b);
+const add = (a,b) => console.log(Number(a)+Number(b));
+const sub = (a,b) => console.log(Number(a)-Number(b));
+const mul = (a,b) => console.log(Number(a)*Number(b));
+const div = (a,b) => console.log(Number(a)/Number(b));
 
 module.exports = {
   add,
